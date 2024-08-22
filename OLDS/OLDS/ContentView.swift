b3c6cfa436f2139f6264e8a59be62dc9e8ecd892
Antara Gupta <antagupta@deloitte.com>
2024-08-22 12:24:26 +0530

    Categories page added

---
+++ OLDS/OLDS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OLDS
+//
+//  Created by Gupta, Antara on 22/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+         
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OLDS/OLDS/OLDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OLDSApp.swift
+//  OLDS
+//
+//  Created by Gupta, Antara on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OLDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Categories()
+        }
+    }
+}
