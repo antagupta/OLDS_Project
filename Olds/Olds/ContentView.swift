1dede6233d918b33f00b0042d0f7c878f6eef3be
Antara Gupta <antagupta@deloitte.com>
2024-08-27 12:15:28 +0530

    Add files via upload

---
+++ Olds/Olds/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Olds
+//
+//  Created by Gupta, Antara on 27/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Olds/Olds/OldsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldsApp.swift
+//  Olds
+//
+//  Created by Gupta, Antara on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OldsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
